484dce8753e9bf022dddeb2437a535aeca81049f
haojie.sun <haojie.sun@yeepay.com>
2021-01-05 15:01:43 +0800

    20210105

---
+++ com.minisun.mycode/one/LargeGroupPositions.java
@@ -0,0 +1,12 @@
+package one;
+
+
+/**
+ * @author yp-tc-m-7151
+ */
+public class LargeGroupPositions{
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
