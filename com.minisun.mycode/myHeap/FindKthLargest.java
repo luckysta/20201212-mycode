a998ff7a17d5549a3890af4488f620104577e256
haojie.sun <18222705672@163.com>
2021-01-16 15:35:58 +0800

    堆： 1.数组中第K个最大元素 2.前K个高频单词

---
+++ com.minisun.mycode/myHeap/FindKthLargest.java
@@ -0,0 +1,26 @@
+package myHeap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class FindKthLargest {
+    public static void main(String[] args) {
+        int[] nums = {3,2,3,1,2,4,5,5,6};
+        int k = 4;
+        System.out.println(findKthLargest(nums,k));
+    }
+
+    public static int findKthLargest(int[] nums,int k){
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((n1,n2)->n2-n1);
+        for (int num : nums) {
+            heap.offer(num);
+        }
+
+        List<Integer> result = new ArrayList<Integer>();
+        while (!heap.isEmpty()){
+            result.add(heap.poll());
+        }
+        return result.get(k-1);
+    }
+}
